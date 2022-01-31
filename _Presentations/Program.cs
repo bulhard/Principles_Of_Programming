61443c22d1506ea9f0c821858c3ca63dc1a6760d
bulhard <pravoslav.milenkov@gmail.com>
2022-01-31 19:35:22 +0200

    Add project files.

---
+++ _Presentations/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _Presentations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Presentations Project");
+        }
+    }
+}
