fa7397c5e46880a60ac2875986ba125370620ea6
bulhard <pravoslav.milenkov@gmail.com>
2022-02-28 12:23:41 +0200

    Classes

---
+++ Lesson_02_Classes/Dog.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Lesson_02_Classes
+{
+    // Class declaration
+    public class Dog
+    { // Opening bracket of the class body
+        // Field declaration
+        private string name;
+
+        // Constructor declaration (peremeterless empty constructor)
+        public Dog()
+        { }
+
+        // Another constructor declaration
+        public Dog(string name)
+        {
+            this.name = name;
+        }
+
+        // Property declaration
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        // Method declaration (non-static)
+        public void Bark()
+        {
+            Console.WriteLine("{0} said: Wow-wow!", name ?? "[unnamed dog]");
+        }
+    } // Closing bracket of the class bod
+}
---
+++ Lesson_02_Classes/DogMeeting.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson_02_Classes
+{
+    public static class DogMeeting
+    {
+        public static void Run()
+        {
+            string firstDogName = null;
+            Console.Write("Enter first dog name: ");
+            firstDogName = Console.ReadLine();
+
+            // Using a constructor to create a dog with specified name
+            Dog firstDog = new Dog(firstDogName);
+
+            // Using a constructor to create a dog wit a default name
+            Dog secondDog = new Dog();
+            Console.Write("Enter second dog name: ");
+            string secondDogName = Console.ReadLine();
+
+            // Using property to set the name of the dog
+            secondDog.Name = secondDogName;
+
+            // Creating a dog with a default name
+            Dog thirdDog = new Dog();
+
+            Dog[] dogs = new Dog[] { firstDog, secondDog, thirdDog };
+
+            foreach (Dog dog in dogs)
+            {
+                dog.Bark();
+            }
+        }
+    }
+}
