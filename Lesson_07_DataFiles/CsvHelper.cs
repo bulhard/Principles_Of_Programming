0ac31975fe9788ea6cea903ac8c22872ef6122fd
bulhard <pravoslav.milenkov@gmail.com>
2022-04-11 22:20:04 +0300

    08

---
+++ Lesson_07_DataFiles/CsvHelper.cs
@@ -0,0 +1,72 @@
+﻿using CsvHelper;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Text;
+
+namespace Lesson_07_DataFiles
+{
+    public static class CsvHelper
+    {
+        public static List<Person> ReadFile(string fileName)
+        {
+            // Create an instance of StreamReader to read from a file
+            StreamReader reader = new StreamReader(fileName);
+            List<Person> result = new List<Person>();
+
+            using (reader)
+            {
+                // Read first line from the text file
+                string line = reader.ReadLine();
+
+                // Read the other lines from the text file
+                while (line != null)
+                {
+                    var lineArr = line.Split(";");
+                    result.Add(new Person
+                    {
+                        FirstName = lineArr[0],
+                        LastName = lineArr[1]
+                    }) ;
+                    line = reader.ReadLine();
+                }
+            }
+
+            return result;
+        }
+
+        public static void WriteIntoFile(List<Person> sourse, string fileName)
+        {
+            // Create a StreamWriter instance
+            StreamWriter writer = new StreamWriter(fileName);
+
+            // Ensure the writer will be closed when no longer used
+            using (writer)
+            {
+                // Loop through the numbers from 1 to 20 and write them
+                foreach(var item in sourse)
+                {
+                    writer.Write(item.FirstName);
+                    writer.Write(";");
+                    writer.Write(item.LastName);
+                    writer.WriteLine();
+                }
+            }
+
+        }
+
+        public static void WriteIntoFile_1(List<Person> sourse, string fileName)
+        {
+            // Create a StreamWriter instance 
+            using (StreamWriter writer = new StreamWriter(fileName))
+            {
+                using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+                {
+                    csv.WriteRecords(sourse);
+                }
+            }
+
+        }
+    }
+}
---
+++ Lesson_07_DataFiles/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Text.Json;
+using System.Xml.Serialization;
+
+namespace Lesson_07_DataFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // CsvHelper.ReadFile("G:\temp\test.txt");
+
+            
+            List<Person> people = GenerateListOfPeople();
+
+            //CsvHelper.WriteIntoFile(people, "G:\\temp\\test.txt");
+
+            //CsvHelper.WriteIntoFile_1(people, "G:\\temp\\test1.txt");
+
+            //var peopleR = CsvHelper.ReadFile("G:\\temp\\test.txt");
+
+            // SerializeDataSet("G:\\temp\\test_xml.txt", people);
+
+            JsonDemo(people);
+        }
+
+        private static void JsonDemo(List<Person> people)
+        {
+            JsonSerializerOptions options = new JsonSerializerOptions();
+            options.WriteIndented = true;
+            options.IgnoreNullValues = true;
+
+            string jsonString = JsonSerializer.Serialize(people, options);
+
+            List<Person> backList = JsonSerializer.Deserialize<List<Person>>(jsonString);
+
+            Console.WriteLine(jsonString);
+        }
+
+        private static List<Person> GenerateListOfPeople()
+        {
+            List<Person> people = new List<Person>();
+
+            for (int i = 1; i <= 10; i++)
+            {
+                people.Add(new Person { 
+                    FirstName = $"FirstName {i}",
+                    LastName = $"LastName {i}"
+                });
+            }
+
+            return people;
+        }
+
+        private static void SerializeDataSet(string filename, List<Person> people)
+        {
+            XmlSerializer ser = new XmlSerializer(typeof(List<Person>));
+
+            TextWriter writer = new StreamWriter(filename);
+
+            ser.Serialize(writer, people);
+
+            writer.Close();
+        }
+    }
+}
